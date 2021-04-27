0a772274f8cefca721e9da33bbca9733f0c31a1d
Winston Robson <43570913+gumdropsteve@users.noreply.github.com>
2021-04-27 00:33:10 -0700

    add basic voting (#8)

--- contracts/Arbitrator.sol
+++ contracts/Arbitrator.sol
@@ -6,6 +6,10 @@ pragma solidity ^0.6.0;
 contract Arbitrator {
     address payable private owner;
 
+    uint256 yeeCount;
+    uint256 nayCount;
+
+
     struct Dispute {
         // the person opening the dispute
         address prosecutor;
@@ -13,6 +17,8 @@ contract Arbitrator {
         address defendant;
         // the amount in $$ that the prosecutor is requesting in damages
         uint256 amount;
+        // response to dispute
+        uint256 response;
         // file hash (stored on ipfs) on prosecutor evidence related to the case
         bytes32 prosecutorEvidence;
         // file hash (stored on ipfs) on defendant evidence related to the case
@@ -20,15 +26,15 @@ contract Arbitrator {
         // status of the current dispute
         disputeStatus status;
         // addresses of users that voted
-        address[] voted;
+        mapping(address => bool) voters;
         // mapping from address of voters to yes or no
         mapping(address => bool) votedYesOrNo;
         // mapping from voters to hashed vote
         mapping(address => uint256)  votedYesOrNoSecret;
         // number of users that voted yes
-        uint yea;
+        uint yeeCount;
         // number of users that voted no
-        uint nay;
+        uint nayCount;
         // date dispute was opened
         uint256 openDate;
         // deadline to vote
@@ -38,7 +44,7 @@ contract Arbitrator {
     }
     Dispute[] public disputes;
 
-    enum disputeStatus {PENDING, CLOSED, GUILTY, INNOCENT, APPEAL}
+    enum disputeStatus {PENDING, CLOSED, VOTING, GUILTY, INNOCENT, APPEAL}
 
     constructor() public {
         owner = msg.sender;
@@ -60,8 +66,8 @@ contract Arbitrator {
         d.prosecutorEvidence = _disputeSummary;
         // set status and voting details
         d.status = disputeStatus.PENDING;
-        d.yea = 0;
-        d.nay = 0;
+        d.yeeCount = 0;
+        d.nayCount = 0;
         d.openDate = today;
         d.voteDeadline = deadline;
         // add dispute to list of disputes
@@ -69,5 +75,39 @@ contract Arbitrator {
         // output this dispute's number for reference
         disputeNumber = disputes.length - 1;
         return disputeNumber;
+    }
+
+    // respond to dispute
+    function respondToDispute(uint256 disputeNumber, uint256 _response) public {
+        require((msg.sender == disputes[disputeNumber].prosecutor) || (msg.sender == disputes[disputeNumber].defendant));
+        disputes[disputeNumber].response = _response;
+        // if response == 0
+        // settleDispute()
+        // elif response == 1
+        // counterDispute()
+        // else
+        // startVote
+        disputes[disputeNumber].status = disputeStatus.VOTING;
+    }
+
+    // vote yee 1 or nay 0
+    function vote(uint256 disputeNumber, bool voteCast) public {
+        require(disputes[disputeNumber].status==disputeStatus.VOTING, "voting not live :)");
+        require(!disputes[disputeNumber].voters[msg.sender], "already voted :)");
+        //if voting is live and address hasn't voted yet, count vote  
+        if(voteCast) {disputes[disputeNumber].yeeCount++;}
+        if(!voteCast) {disputes[disputeNumber].nayCount++;}
+        //address has voted, mark them as such
+        disputes[disputeNumber].voters[msg.sender] = true;
+    }
+
+    //Outputs current vote counts
+    function getVotes(uint256 disputeNumber) public view returns (uint yesVotes, uint noVotes) {
+        return(disputes[disputeNumber].yeeCount, disputes[disputeNumber].nayCount);
+    }
+
+    //Lets user know if their vote has been counted
+    function haveYouVoted(uint256 disputeNumber) public view returns (bool) {
+        return disputes[disputeNumber].voters[msg.sender];
     }
 }
